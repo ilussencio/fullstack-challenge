86aa5aaf6e65964fc5625aea8bd3770d4e5add6a
Lucas Ilussencio Silva <ilussencio@gmail.com>
2024-11-27 20:00:14 -0300

    updating the base project

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+
+
+
+
+export default defineConfig(({mode}) =>{
+    const env = loadEnv(mode, process.cwd(), '')
+
+    return {base : "/" ,
+    plugins: [react()],
+    server: {
+        port: 8081,
+        strictPort: true,
+        host: true,
+        origin: "http://0.0.0.0:8081",
+    },
+    preview: {
+        port: 8081,
+        strictPort: true,
+    },
+    define: {
+        __GOOGLE_API__: JSON.stringify(env.GOOGLE_API_KEY)
+    }}
 })
